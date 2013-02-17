a97a987933180bcfbecbcee792239614352e2ca8
slamet <slametnhd@gmail.com>
2013-02-16 18:27:34 -0800

    deploy to heroku with css

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,13 @@
+class PageController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def help
+  end
+
+  def contact
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aplikasi::Application.config.secret_token = '4bb90f221c245f86fb94a15e94ffe5cd7b66d635df781bbddf3d091e4b2233c4f908de42d247570c6da3c1467f47810c4f6c4abee6719c29cb87e6b2fb4a43d0'
